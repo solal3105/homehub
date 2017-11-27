b71751e8bf2f29011be9df47aed73a8fc071f0f9
Jean-Baptiste Loutfalla <33581711+ShadowRises@users.noreply.github.com>
2017-11-27 13:34:45 +0000

    Update humidity.py

--- python/humidity.py
+++ python/humidity.py
@@ -1,7 +1,8 @@
 from sense_hat import SenseHat
 
 sense = SenseHat()
-
+sense.clear()
 humidity = sense.get_humidity()
-
-print humidity
+humidity = round(humidity, 1)
+sense.show_message(str(humidity))
+print(humidity)
