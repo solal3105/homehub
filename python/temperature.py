98ff9101ddf22a65e15acf1627a56bdaa3ee8a5e
Jean-Baptiste Loutfalla <jb.loutfalla@orange.fr>
2017-12-11 15:24:23 +0100

    Modifications of the python files

--- python/temperature.py
+++ python/temperature.py
@@ -1,7 +1,9 @@
 from sense_hat import SenseHat
 
 sense = SenseHat()
-
+sense.clear()
 temperature = sense.get_temperature()
-
-print temperature
+temperature = round(humidity, 1)
+file = open("temperature.txt", "w")
+file.write(temperature)
+file.close()
