dc239b46c8885bdca30bf10e9eaf0680579ad9e1
solal gendrin <adresse3105@hotmail.fr>
2017-12-11 15:08:53 +0100

    every views

--- controllers/chat.php
+++ controllers/chat.php
@@ -12,6 +12,6 @@ elseif ($_SESSION['name']=='Marc') {
 elseif ($_SESSION['name']=='Jean-B') {
  $iframe="<iframe width=\"500\" height=\"500\" src=\"http://www.calculator-1.com/outdoor/?f=000000&r=000000\" scrolling=\"no\" frameborder=\"0\"></iframe>";
 }
-require_once(PATH_VIEWS.'maps.php');
+require_once(PATH_VIEWS.'chat.php');
 
 ?>
--- views/applications.php
+++ views/applications.php
@@ -7,34 +7,23 @@
 <body>
 	<div id="container">
 	<ul id="colonne_gauche">  
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-pencil" aria-hidden="true"></i></li> </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-envelope-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-headphones" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-comments-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-calculator" aria-hidden="true"></i></li></a>
-		<a href="?page=maps"></a><li class="flex-item"><i class="fa fa-map" aria-hidden="true"></i></li> </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-envelope-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-headphones" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-comments-o" aria-hidden="true"></i></li>  </a>
-		<a href="?page="></a><li class="flex-item"><i class="fa fa-calculator" aria-hidden="true"></i></li></a>
+		<a href="?page=docs"><li class="flex-item"><i class="fa fa-pencil" aria-hidden="true"></i></li> </a>
+		<a href="?page=mail"><li class="flex-item"><i class="fa fa-envelope-o" aria-hidden="true"></i></li>  </a>
+		<a href="?page=keep"><li class="flex-item"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></li>  </a>
+		<a href="?page=spotify"><li class="flex-item"><i class="fa fa-headphones" aria-hidden="true"></i></li>  </a>
+		<a href="?page=chat"><li class="flex-item"><i class="fa fa-comments-o" aria-hidden="true"></i></li>  </a>
+		<a href="?page=calculator"><li class="flex-item"><i class="fa fa-calculator" aria-hidden="true"></i></li></a>
+		<a href="?page=maps"><li class="flex-item"><i class="fa fa-map" aria-hidden="true"></i></li> </a>
 	</ul>
 	<div id="colonne_droite">
 		<h1>Hello, <?= $_SESSION['name']; ?></h1>
-		<div class="sensor">
-			<p><i class="fa fa-tint" aria-hidden="true"></i>
-				<?= $humidity ?>%</p>
-		</div>
-		<div class="sensor">
-			<p><i class="fa fa-tachometer" aria-hidden="true"></i>
-				<?= $pressure ?>bar</p>
-		</div>
-		<div class="sensor">
-			<p><i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i>
-				<?= $temperature ?>°c
-			</p>
-		</div>
+		<p><i class="fa fa-tint" aria-hidden="true"></i>
+		<?= $humidity ?>%</p>
+		<p><i class="fa fa-tachometer" aria-hidden="true"></i>
+		<?= $pressure ?>bar</p>
+		<p><i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i>
+		<?= $temperature ?>°c
+		</p>
 	</div>
 	</div>
 </body>
